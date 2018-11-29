057ce7b0ca1e4ca3d7e7c4949a41a46c325997d2
Fitz1003Miao <43803910+Fitz1003Miao@users.noreply.github.com>
2018-11-29 19:43:39 +0800

    update

--- AlexNet/src/train.py
+++ AlexNet/src/train.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 import os
 import AlexNet
 from PIL import Image
+import argparse
 
 CLASS_LIST = ['cat', 'dog']
 
@@ -35,9 +36,63 @@ def weight_init(m):
         m.weight.data.fill_(1)
         m.bias.data.zero_()
 
+def valmodel(model, dataloader, criterion, use_gpu):
+    model.eval()
+
+    total_loss = 0
+    total_correct = 0
+
+    for batch_idx, (data, target) in enumerate(dataloader):
+        if use_gpu:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        output = model(data)
+        
+        loss = criterion(output, target)
+        total_loss += loss.item()
+        
+        pred = torch.max(F.softmax(output), 1)[1]
+        total_correct += (pred == target).sum().item()
+
+    print("Val: Loss: {:.6f}, Acc: {:.6f}".format(total_loss / len(dataloader), total_correct / len(dataloader.dataset)))
+
+def trainmodel(model, dataloader, criterion, optimizer, use_gpu):
+    model.train()
+    for batch_idx, (data, target) in enumerate(dataloader):
+        if use_gpu:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+
+        output = model(data)
+        pred = torch.max(F.softmax(output), 1)[1]
+        loss = criterion(output, target)
+
+        loss.backward()
+        optimizer.step()
+
+        print('Train: [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccu: {:.6f}'.format(
+                batch_idx * len(data), len(dataloader.dataset),
+                100. * batch_idx / len(dataloader), loss.item(), (target == pred).sum().item() / target.size(0)))
+
+def savemodel(model, optimizer, outpath):
+    torch.save({'model_state_dict' : model.state_dict(), 'optimizer_state_dict' : optimizer.state_dict(), 'epoch' : outpath}, outpath)
+
 def main():
-    train_dataset = MyDataSet('../data/train')
-    val_dataset = MyDataSet('../data/val')
+    parse = argparse.ArgumentParser()
+    parse.add_argument('-t', '--train', help = 'Path to train data')
+    parse.add_argument('-v', '--val', help = 'Path to val data')
+    parse.add_argument('-o', '--outpath', help = 'Path to save model')
+    parse.add_argument('-m', '--model', help = 'Path to load model')
+
+    args = parse.parse_args()
+    print(args)
+
+    train_dataset = MyDataSet(args.train)
+    val_dataset = MyDataSet(args.val)
+
+    if not os.path.exists(args.outpath):
+        os.makedirs(args.outpath, exist_ok = True)
 
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size = 128, shuffle = True, num_workers = 0)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size = 128, shuffle = False, num_workers = 0)
@@ -52,36 +107,17 @@ def main():
         print('USE CPU')
 
     criterion = nn.CrossEntropyLoss(size_average = True)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.99))
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay = 0.0005)
+
+    if args.model is not None:
+        checkpoint = torch.load(args.model)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        print(checkpoint['epoch'])
 
     for epoch in range(1, 100):
-        model.train()
-        for batch_idx, (data, target) in enumerate(train_dataloader):
-            if use_gpu:
-                data, target = data.cuda(), target.cuda()
-            data, target = Variable(data), Variable(target)
-            optimizer.zero_grad()
-            output = model(data)
-            pred = torch.max(F.softmax(output), 1)[1]
-            loss = criterion(output, target)
-            loss.backward()
-            optimizer.step()
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccu: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_dataloader.dataset),
-                    100. * batch_idx / len(train_dataloader), loss.data[0], (target == pred).sum().item() / target.size(0)))
-
-            # if batch_idx % 10 == 0 and batch_idx != 0:
-            #     val_total_loss = 0
-            #     val_pred_correct = 0
-            #     for batch_val_idx, (val_data, val_target) in enumerate(val_dataloader):
-            #         if use_gpu:
-            #             val_data, val_target = val_data.cuda(), val_target.cuda()
-            #         val_output = model(val_data)
-            #         val_pred = torch.max(F.softmax(val_output), 1)[1]
-            #         val_loss = criterion(val_output, val_target)
-            #         val_total_loss += val_loss
-            #         val_pred_correct += (val_target == val_pred).sum().item()
-            #     print('Val: Loss: {:.6f}, Acc: {:.6f}'.format(val_total_loss / len(val_dataloader), val_pred_correct / val_dataset.__len__()))
-
+        trainmodel(model, train_dataloader, criterion, optimizer, use_gpu)
+        valmodel(model, val_dataloader, criterion, use_gpu)
+        savemodel(model, optimizer, os.path.join(args.outpath, 'iter-{:04d}'.format(epoch)))
 if __name__ == "__main__":
     main()
